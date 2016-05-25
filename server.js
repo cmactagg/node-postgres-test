75b49b20630fe685e2126dbb46466b7c79dfc995
mtags <mtags22@gmail.com>
2016-05-25 09:00:01 -0600

    added the code to connect to the db and some documentation

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("working");
+var pgp = require('pg-promise')();
+
+var cn = 'postgres://postgres:password@localhost:5432/node-postgres-test';
+
+var db = pgp(cn);
+
+db.any("select * from mytest")
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (error) {
+        // error;
+    });
+
+
+
+console.log("Running");
